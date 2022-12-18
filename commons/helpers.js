fa2c9a253c11995bb19d4741790c53c01810ec36
JustDNA <dhiwakar.nitt@gmail.com>
2022-12-18 14:53:54 +0530

    add docs to helper method

--- commons/helpers.js
+++ commons/helpers.js
@@ -1,5 +1,14 @@
 var cronParser = require('cron-parser');
 
+/**
+ * @method getLastTimeWindowForCron
+ * @description Given cron expression, this method returns
+ * the last time window. For example, the cron represents an hourly
+ * frequency, at 06:01:37, this would return the timestamp equivalent
+ * for the window [05:00:00, 06:00:00]
+ * @param {String} cronExpression 
+ * @returns [startTimestamp, endTimestamp]
+ */
 exports.getLastTimeWindowForCron = function(cronExpression) {
     const interval = cronParser.parseExpression(cronExpression);
     interval.prev();
